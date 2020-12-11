1c3ce3ab469176733ba9a0368ff036ed9624d089
Bela <belahamid@gmail.com>
2020-12-11 22:22:04 +0000

    setting up folders and files for models and tests

---
+++ tests/activity_test.py
@@ -0,0 +1,6 @@
+import unittest
+
+from models.activity import Activity
+
+class TestActivity(unittest.TestCase):
+    pass
---
+++ tests/member_test.py
@@ -0,0 +1,5 @@
+import unittest
+from models.member import Member
+
+class TestMember(unittest.TestCase):
+    pass
