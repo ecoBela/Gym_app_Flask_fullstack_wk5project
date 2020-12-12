831e56097cf1ddb6ae69c4a47e83a5e823bf6122
Bela <belahamid@gmail.com>
2020-12-12 00:46:14 +0000

    added pseudocode

---
+++ db/gym.sql
@@ -0,0 +1,8 @@
+-- DROP TABLE IF EXISTS bookings
+-- DROP TABLE IF EXISTS activities
+-- DROP TABLE IF EXISTS members
+
+
+-- CREATE TABLE members();
+-- CREATE TABLE activities():
+-- CREATE TABLE bookings();
