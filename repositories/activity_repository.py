45a94ed90f4859ad8070c9eecb050c7e1cc342df
Bela <belahamid@gmail.com>
2020-12-12 08:22:30 +0000

    added imports to member_repository.py

---
+++ repositories/activity_repository.py
@@ -0,0 +1,17 @@
+from db.run_sql import run_sql
+from models.activity import Activity
+from models.member import Member
+
+
+# CRUD: create
+# def save()
+
+#CRUD: read
+#Do we need a def select() here?
+
+
+#CRUD: update
+# def update()
+
+#CRUD: delete
+#delete_all()
---
+++ repositories/member_repository.py
@@ -0,0 +1,16 @@
+from db.run_sql import run_sql
+from models.member import Member
+from models.activity import Activity
+
+# CRUD: create
+# def save()
+
+#CRUD: read
+#Do we need a def select() here?
+
+
+#CRUD: update
+# def update()
+
+#CRUD: delete
+#delete_all()
