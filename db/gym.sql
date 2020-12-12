2a4a88d9a77f6b4ee9162c6ed4de81e4adbbd73d
Bela <belahamid@gmail.com>
2020-12-12 01:06:15 +0000

    created tables in db

--- db/gym.sql
+++ db/gym.sql
@@ -1,8 +1,20 @@
--- DROP TABLE IF EXISTS bookings
--- DROP TABLE IF EXISTS activities
--- DROP TABLE IF EXISTS members
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS members;
 
 
--- CREATE TABLE members();
--- CREATE TABLE activities():
--- CREATE TABLE bookings();
+CREATE TABLE members(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) 
+);
+
+CREATE TABLE activities(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    upcoming BOOLEAN
+);
+CREATE TABLE bookings(
+    id SERIAL PRIMARY KEY,
+    member_id INT REFERENCES members(id) ON DELETE CASCADE,
+    activity_id INT REFERENCES activities(id) ON DELETE CASCADE
+);
