b0d3e95bcc2fbfbda04fd4a2ab99d44201aa9276
Bela <belahamid@gmail.com>
2020-12-14 21:16:28 +0000

    Added link to Homepage to take user to Members page, added link to members page to take user to Create New Member page

---
+++ controllers/activity_controller.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+# INDEX
+# /activities, GET,
+
+# NEW
+# /activities/new, GET
+
+# CREATE
+# /activities, POST,
+
+# SHOW
+# /activities/id, GET
+
+# EDIT
+# /activities/edit, GET
+
+# UPDATE
+# /activities/id, PUT
