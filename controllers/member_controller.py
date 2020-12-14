cb758a221a8004111d28beece9465448c2308aad
Bela <belahamid@gmail.com>
2020-12-14 21:53:45 +0000

    Create new member: user journey complete. New member is added to the database

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.member import Member
 import repositories.member_repository as member_repository 
@@ -6,15 +6,11 @@ import repositories.member_repository as member_repository
 members_blueprint = Blueprint("members", __name__)
 
 #INDEX
-
-#/members: GET, will render index.html page displaying options
 @members_blueprint.route("/members")
 def members():
     return render_template("members/index.html")
 
 # NEW
-
-#/members/new: GET, will render new.html page displaying form
 @members_blueprint.route("/members/new")
 def new_member():
     return render_template("members/new.html")
@@ -22,8 +18,20 @@ def new_member():
 #CREATE
 
 #/members: POST, will add member to db but should redirect user to home page afterwards
-# @members_blueprint.route("/members" methods=["POST"])
-# def create_member():
+@members_blueprint.route("/members", methods=["POST"])
+def create_member():
+    name = request.form["name"]
+    new_member = Member(name)
+    member_repository.save(new_member)
+    return redirect ("/")
+    
+
+
+# def create_human():
+#     name = request.form["name"]
+#     new_human = Human(name)
+#     human_repository.save(new_human)
+#     return redirect("/humans")
     
 # SHOW
 
