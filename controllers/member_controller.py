17cdc0cdf40fa444bcccad113721391a5279e041
Bela <belahamid@gmail.com>
2020-12-14 16:21:13 +0000

    added comments to help me with required routes in member_controller.py

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -3,3 +3,18 @@ from flask import Blueprint
 from models.member import Member
 
 members_blueprint = Blueprint("members", __name__)
+
+#routes
+
+#/members: GET, will render index.html page displaying options
+
+
+#/members/new: GET, will render new.html page displaying form
+
+#/members: POST, will add member to db but should redirect user to home page afterwards
+
+#/members/id: GET, will render the show.html page displaying member
+
+#/members/edit: GET, will render edit.html page displaying form
+
+#/members/id: PUT will add edits to db but should redirect user to home page afterwards
