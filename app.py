cc79d98704bc1c7e076dd2f050f17f82b6a9de52
Bela <belahamid@gmail.com>
2020-12-16 14:25:25 +0000

    changed controller names to plurals

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from controllers.member_controller import members_blueprint
-from controllers.activity_controller import activities_blueprint
+from controllers.members_controller import members_blueprint
+from controllers.activities_controller import activities_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(members_blueprint)
