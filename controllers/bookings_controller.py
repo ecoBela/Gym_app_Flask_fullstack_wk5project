65836a445b3c1d9c7a4cb1b30e105b722282d97f
Bela <belahamid@gmail.com>
2020-12-17 02:05:12 +0000

    started /bookings route, and created template files

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -4,3 +4,21 @@ from models.booking import Booking
 import repositories.booking_repository as booking_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
+
+
+#INDEX: GET, return index.html, /bookings route
+@bookings_blueprint.route("/bookings")
+def bookings():
+    return render_template('bookings/index.html', bookings=bookings)
+
+
+#NEW
+
+#CREATE
+
+#SHOW
+
+#EDIT
+
+
+#UPDATE
